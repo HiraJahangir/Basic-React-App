3b26cdaca24301aca94a13428a9d25f8e85a0343
HiraJahangir <hirajahangir93@gmail.com>
2020-06-27 17:53:26 +0500

    create basic react app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Hello from './Hello';
+import './Hello.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function App({name, age}) {
+return <div align='center'>
+        <p className='myname1'>Hello from <strong>{name}</strong> Age={age-4}
+        </p> 
+        <br/>
+        <h1 className='myname2'>Learning Basics of React</h1>
+        <h2 className='myname2'>Creating First React App</h2>
+        <h3 className='body-text'>Languages:
+        <ol>
+          <li>C++</li>
+          <li>Python</li>
+          <li>Visual Basic</li>
+        </ol>
+        </h3>
+        <h3 className='body-text'>Skills:  
+        <ul>
+          <li>WordPress</li>
+          <li>Content Writing</li>
+          <li>Account Managment</li>
+        </ul>
+        </h3>
+        <Hello firstName={name}></Hello>
+      </div>
+  }
 
 export default App;
